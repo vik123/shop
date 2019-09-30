d847a1dd5d29f176683cdd6cf91797f7a7587420
vik123 <vikrawat11@gmail.com>
2019-09-30 22:31:42 +0530

    add haeder section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { BrowserRouter, Route} from 'react-router-dom';
+import { BrowserRouter, Route, Switch} from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 const Hats = () => (
   <div>Hats</div>
@@ -13,9 +14,12 @@ function App() {
   return (
     <BrowserRouter >
       <div>
-        <Route exact path='/' component={HomePage} />
-        <Route exact path='/shop' component={ShopPage} />
-        <Route exact path='/shop/hats' component={Hats} />
+        <Header />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route exact path='/shop' component={ShopPage} />
+          <Route exact path='/shop/hats' component={Hats} />
+        </Switch>
       </div>
     </BrowserRouter>
   );
